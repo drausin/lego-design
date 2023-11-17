9d5e68ec9ce8db78a65db2b9117bd49af5c9720b
Drausin Wulsin <daedalus2718@gmail.com>
2023-11-16 20:53:50 -0500

    Add ldraw_part_images and catalog_db fetch functions

--- src/data/fetch.py
+++ src/data/fetch.py
@@ -4,6 +4,7 @@ import os
 import time
 import zipfile
 
+from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -25,6 +26,39 @@ def ldraw_parts(data_dir: str):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(data_dir)
     print(f"Extracted {len(zip_ref.namelist())} files")
+
+
+def ldraw_part_images(data_dir: str):
+    url = 'https://rebrickable.com/downloads/'
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure the request was successful
+
+    images_dir = os.path.join(data_dir, 'ldraw_images')
+    os.makedirs(images_dir, exist_ok=True)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a', href=True)
+
+    for link in links:
+        href = link['href']
+        if href.endswith('.zip'):
+            try:
+                download_response = requests.get(href)
+                download_response.raise_for_status()
+
+                filename = href.split('/')[-1]
+                file_path = os.path.join(images_dir, filename)
+
+                with open(file_path, 'wb') as file:
+                    file.write(download_response.content)
+                print(f'Downloaded {filename} to {file_path}')
+
+                with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                    zip_ref.extractall(images_dir)
+                print(f"Extracted {len(zip_ref.namelist())} files")
+
+            except Exception as e:
+                print(f'Failed to download {href}: {e}')
 
 
 def ldraw_models(data_dir: str):
@@ -96,13 +130,51 @@ def ldraw_models(data_dir: str):
         driver.quit()
 
 
+def catalog_db(data_dir: str):
+    url = 'https://rebrickable.com/downloads/'
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure the request was successful
+
+    catalog_dir = os.path.join(data_dir, 'catalog_db')
+    os.makedirs(catalog_dir, exist_ok=True)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a', href=True)
+
+    for link in links:
+        href = link['href']
+        if 'csv.gz' in href:
+            try:
+                download_response = requests.get(href)
+                download_response.raise_for_status()
+
+                filename = href.split('/')[-1].split('?')[0]  # Remove URL parameters
+                file_path = os.path.join(catalog_dir, filename)
+
+                with open(file_path, 'wb') as file:
+                    file.write(download_response.content)
+                print(f'Downloaded {filename} to {file_path}')
+            except Exception as e:
+                print(f'Failed to download {href}: {e}')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", choices=["ldraw-parts", "ldraw-models"], help="The command to run")
+    parser.add_argument(
+        "command",
+        choices=["ldraw-parts", "ldraw-part-images", "ldraw-models", "catalog-db"],
+        help="The command to run",
+    )
     parser.add_argument("--data-dir", default="./data", help="The local data directory")
     args = parser.parse_args()
 
     if args.command == "ldraw-parts":
         ldraw_parts(args.data_dir)
+    elif args.command == "ldraw-part-images":
+        ldraw_part_images(args.data_dir)
     elif args.command == "ldraw-models":
         ldraw_models(args.data_dir)
+    elif args.command == "catalog-db":
+        catalog_db(args.data_dir)
+    else:
+        print(f"unexpected command: {args.command}")
